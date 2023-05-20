4418398cc4c34662828d167ae6068d0327fc20e7
jpmedinagl <jpmedinagl@gmail.com>
2023-05-20 11:37:09 -0400

    complete main

--- src/main/java/BagMain.java
+++ src/main/java/BagMain.java
@@ -17,10 +17,10 @@ class BagMain {
     public static void enhanceBags(Bag[] bags, boolean double_enhance_handbags) {
         // TODO: Implement this.
         for (Bag bag: bags) {
-            bag.enhance();
-        }
-        if (double_enhance_handbags) {
-            for (Bag bag: bags) {
+            if (double_enhance_handbags && bag instanceof HandBag) {
+                bag.enhance();
+                bag.enhance();
+            } else {
                 bag.enhance();
             }
         }
